9ee8b4c657b4547531bb68f4a701f42f49bf2f66
Intans <ggwpg32@mhs.mdp.ac.id>
2023-10-27 18:41:55 +0700

    MembuatSignIn

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/SignInScreen.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
 import 'package:wisata_candi/screens/profile_screen.dart';
 
@@ -15,12 +16,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Wisata Candi',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       // home: DetailScreen(candi: candiList[0]),
-      home: ProfileScreen(),
+      home: SignInScreen(),
     );
   }
 }
--- wisata_candi/lib/screens/profile_screen.dart
+++ wisata_candi/lib/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/widget/profile_info_item.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -14,56 +15,116 @@ class _ProfileScreenState extends State<ProfileScreen> {
   String userName = "";
   int favoriteCandiCount = 0;
 
-  @override
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
+        alignment: Alignment.topCenter,
         children: [
           Container(
             height: 200,
             width: double.infinity,
-            color: Colors.deepPurple,
+            color: Colors.amber,
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              children: [
-                // TODO: 2,buat bagian ProfileHeader yang berisi foto profil
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 200 - 50),
-                    child: Stack(
-                      alignment: Alignment.bottomRight,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                                  color: Colors.deepOrange, width: 2),
-                              shape: BoxShape.circle),
-                          child: CircleAvatar(
-                            radius: 50,
-                            backgroundImage:
-                                AssetImage("images/placeholder_image.png"),
-                          ),
+          Column(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 200 - 50),
+                  child: Stack(
+                    alignment: Alignment.bottomRight,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            border:
+                                Border.all(color: Colors.deepPurple, width: 2),
+                            shape: BoxShape.circle),
+                        child: const CircleAvatar(
+                          radius: 50,
+                          backgroundImage:
+                              AssetImage('images/placeholder_image.png'),
                         ),
-                        if (isSignIn)
-                          IconButton(
-                              onPressed: () {},
-                              icon: Icon(
-                                Icons.camera_alt,
-                                color: Colors.deepPurple[50],
-                              ))
-                      ],
-                    ),
+                      ),
+                     
+                    ],
                   ),
-                )
-              ],
-
-              // TODO: 3, Buat bagian profileFoto yang berisi info profil
-              // TODO: 4, buat bagian profileAction yang berisi TextButton signin / signout
-            ),
-          ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Divider(
+                      color: Colors.deepPurple[100],
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    ProfileInfoItem(
+                      icon: Icons.lock,
+                      label: 'Pengguna',
+                      value: userName,
+                      iconColor: Colors.amber,
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Divider(
+                      color: Colors.deepPurple[100],
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    ProfileInfoItem(
+                        icon: Icons.person,
+                        label: 'Nama',
+                        value: fullName,
+                        onEditPressed: () {
+                          debugPrint('Icon edit ditekan ...');
+                        },
+                        iconColor: Colors.blue),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Divider(
+                      color: Colors.deepPurple[100],
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    ProfileInfoItem(
+                        icon: Icons.favorite,
+                        label: 'Favorit',
+                        value:
+                            favoriteCandiCount > 0 ? '$favoriteCandiCount' : '',
+                        iconColor: Colors.red),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Divider(
+                      color: Colors.deepPurple[100],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                         TextButton(
+                            onPressed: () {},
+                            child: Text('Sign Out'),
+                            style: TextButton.styleFrom(
+                                backgroundColor: Colors.amber,
+                                padding: const EdgeInsets.all(20),
+                                elevation: 5),
+                          )
+                         
+                  ],
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
