df6288a10d9d41b3317fdcfafc65a620f3664b14
MichaelRodaRobert <96275174+MichaelRodaRobert@users.noreply.github.com>
2023-12-08 18:37:25 +0700

    Hero I

--- wisata_candi/lib/widget/item_card.dart
+++ wisata_candi/lib/widget/item_card.dart
@@ -16,53 +16,56 @@ class ItemCard extends StatelessWidget {
           context,
           MaterialPageRoute(
             builder: (context) => DetailScreen(candi: candi),
-            ),
-          );
+          ),
+        );
       },
-    child: Card(
-      // TODO: 2. Tetapkan parameter shape, margin, dan elevation dari Cari
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
-      margin: const EdgeInsets.all(4),
-      elevation: 1,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // TODO: 3 Buat Image sebagai anak dari Column
-          Expanded(
-            // TODO: 7. Implementasi Hero animation
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: Image.asset(
-                candi.imageAsset,
-                width: double.infinity,
-                fit: BoxFit.cover,
+      child: Card(
+        // TODO: 2. Tetapkan parameter shape, margin, dan elevation dari Cari
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        margin: const EdgeInsets.all(4),
+        elevation: 1,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // TODO: 3 Buat Image sebagai anak dari Column
+            Expanded(
+              // TODO: 7. Implementasi Hero animation
+              child: Hero(
+                tag: candi.imageAsset,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.asset(
+                    candi.imageAsset,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
             ),
-          ),
-          // TODO: 4 Buat Text sebagai anak dari Column
-          Padding(
-            padding: const EdgeInsets.only(left: 16, top: 8),
-            child: Text(
-              candi.name,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+            // TODO: 4 Buat Text sebagai anak dari Column
+            Padding(
+              padding: const EdgeInsets.only(left: 16, top: 8),
+              child: Text(
+                candi.name,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          // TODO: 5 Buat Text sebagai anak dari Column
-          Padding(
-            padding: const EdgeInsets.only(left: 16, bottom: 8),
-            child: Text(
-              candi.type,
-              style: const TextStyle(fontSize: 12),
-            ),
-          )
-        ],
+            // TODO: 5 Buat Text sebagai anak dari Column
+            Padding(
+              padding: const EdgeInsets.only(left: 16, bottom: 8),
+              child: Text(
+                candi.type,
+                style: const TextStyle(fontSize: 12),
+              ),
+            )
+          ],
+        ),
       ),
-     ),
     );
   }
 }
